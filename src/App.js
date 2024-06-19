a266fa893ec04e30a9c2205ee69ab3463da656c8
MartinSal1 <martysalwork@gmail.com>
2024-06-19 13:03:53 -0400

    app, index, html basic setup

---
+++ src/App.js
@@ -0,0 +1,85 @@
+export default function Board() {
+  return <div className="Board">
+  <div className="row-1">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-2">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-3">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-4">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-5">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-6">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-7">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+  <div className="row-8">
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </div>
+
+  </div>;
+}
